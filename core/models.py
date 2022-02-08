58f14c761a9ab433e190a551b5c56ce1151a70e3
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-09 00:12:03 +0330

    implement BaseModel in core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class BaseModel(models.Model):
+    create_timestamp = models.DateTimeField(auto_now_add=True)
+    update_timestamp = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    def activate(self):
+        self.is_active = True
+        self.save()
+
+    def deactivate(self):
+        self.is_active = False
+        self.save()
+
+    def logical_delete(self):
+        self.is_deleted = True
+        self.save()
+
+    class Meta:
+        abstract = True
