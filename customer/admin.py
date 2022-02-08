0b4b4912381fd56ae73c9e6fc38c067b351930fc
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-09 00:52:14 +0330

    add Customer to Admin

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'phone']
+    list_display_links = ['username', 'email']
+
+    def logical_delete(self, request, queryset):
+        queryset.update(is_deleted=True)
+
+    actions = ['logical_delete']
+
+
+admin.site.register(Customer, CustomerAdmin)
