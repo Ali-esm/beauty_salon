2c65eca4cebc666cb09d31c2ab1b850c621dee84
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-11 20:49:23 +0330

    add Service & agent to admin

--- salon/admin.py
+++ salon/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Service, Agent
 
-# Register your models here.
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'is_deleted', 'is_active']
+    list_editable = ['price']
+    list_filter = ['price']
+
+
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'experience_year', 'age', 'services_count']
+
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Agent, AgentAdmin)
