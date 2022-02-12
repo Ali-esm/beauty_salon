ca5f423076030317548fa3958de3ef9a2f7ebcda
Ali.es <ali.esmailnejad96@gmail.com>
2022-02-12 15:26:03 +0330

    add BookingForm to admin

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from .forms import BookingForm
 from .models import Booking, Holiday
 
-admin.site.register(Booking)
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'agent', 'service', 'reserve_time', 'reserve_date']
+    form = BookingForm
+
+
+admin.site.register(Booking, BookingAdmin)
 admin.site.register(Holiday)
