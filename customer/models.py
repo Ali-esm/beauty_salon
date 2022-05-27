1817c66470078215fc4d70e23612ec7cb58e1282
Ali.es <ali.esmailnejad96@gmail.com>
2022-05-27 16:44:01 +0430

    [style]: remove additional comment

--- customer/models.py
+++ customer/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from core.models import BaseModel
 
 
-# Create your models here.
 class Customer(BaseModel):
     username = models.CharField(max_length=30)
     email = models.CharField(max_length=100)
